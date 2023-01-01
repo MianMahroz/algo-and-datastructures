d30bf52bbb828bbeabb4c407182b069fdd01baac
mahroz.naeem <mianmahroz1@gmail.com>
2023-01-01 23:10:53 +0400

    test_sort_integer_array

--- algo-using-kotlin/src/test/kotlin/CommonAlgoTest.kt
+++ algo-using-kotlin/src/test/kotlin/CommonAlgoTest.kt
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class CommonAlgoTest {
 
     @Test
-    fun test_find_first_And_second_max_number():Unit{
+    fun test_find_first_And_second_max_number(){
 
         // find the first max number
         val randomIntegerArr = listOf(2,5,6,9,12,100)
@@ -26,8 +27,8 @@ class CommonAlgoTest {
             }
         }
 
-        assertEquals(12,secondMaxNumber);
-        assertEquals(100,maxNumber);
+        assertEquals(12,secondMaxNumber)
+        assertEquals(100,maxNumber)
     }
 
 
@@ -37,7 +38,7 @@ class CommonAlgoTest {
     fun test_find_min_ops(){
 
         var w1 = "intention"
-        var w2  = "execution"
+        val w2  = "execution"
 
         var count  = 0
 
@@ -48,7 +49,7 @@ class CommonAlgoTest {
                 // check if that char is available in w1
                 // if not insert new or get from w2
                 if(w1.contains(w2[i])){
-                    var missingChar =  w1[w1.indexOf(w2[i])]
+                    val missingChar =  w1[w1.indexOf(w2[i])]
                     w1 = w1.replaceRange(i,(i+1), missingChar.toString())
                     count ++
                 }else {
@@ -63,12 +64,27 @@ class CommonAlgoTest {
             }
         }
 
-
         assertEquals(w2,w1)
 
+    }
 
 
+    @Test
+    fun test_sort_integer_array(){
 
+        val numberList = mutableListOf(4,1,2,3,5)
+
+        for (i in numberList.indices){
+            for (j in i until numberList.size){
+                if(numberList[i] > numberList[j]){
+                    val temp = numberList[i]
+                    numberList[i] = numberList[j]
+                    numberList[j] = temp
+                }
+            }
+        }
+
+        assertContentEquals(mutableListOf(1,2,3,4,5), numberList)
 
     }
 
