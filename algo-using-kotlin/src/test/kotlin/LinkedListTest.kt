7b0e7f9f4647809c69b585339894d59463136865
mahroz.naeem <mianmahroz1@gmail.com>
2023-01-12 00:41:15 +0400

    test_removeDuplicates added

--- algo-using-kotlin/src/test/kotlin/LinkedListTest.kt
+++ algo-using-kotlin/src/test/kotlin/LinkedListTest.kt
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class Node (var next:Node?, var data:Int){
@@ -30,5 +30,75 @@ class LinkedListTest {
     }
 
 
+
+    @Test
+    fun test_deleteNode(){
+
+        val myLinkedList  =  Node(Node(Node(null,6),4),2)
+
+        var current = myLinkedList
+        while (current.next != null){
+
+            if(current.next!!.data==4){
+                current.next = current.next!!.next
+            }
+                current = current.next!!
+        }
+
+        assertTrue(myLinkedList.next?.data==6)
+
+    }
+
+
+    @Test
+    fun reverseLinkedList(){
+
+        var node = Node(Node(Node(null,6),4),2)
+
+        var current:Node? = node
+        var prev:Node? = null
+        var next:Node? = null
+
+        while (current != null){
+
+            next = current.next
+            current.next = prev // points to null on first iteration
+            prev = current
+
+            current = next // move pointer for next iteration
+        }
+
+        node = prev!!
+
+        assertTrue(node.data == 6)
+    }
+
+    @Test
+    fun test_removeDuplicates(){
+
+        val node = Node(Node(Node(Node(null,6),2),4),2)
+
+        val dataStore = mutableListOf<Int?>()
+
+        var current:Node? = node
+
+        // insert first element
+        dataStore.add(node.data)
+
+        // print node data
+        while (current?.next!=null){
+
+            if(!dataStore.contains(current.next?.data)){
+                dataStore.add(current.next?.data)
+            }else{
+                current.next = current.next?.next
+            }
+            current = current.next
+        }
+
+        assertEquals(node.next?.next?.data,6)
+
+
+    }
 
 }
